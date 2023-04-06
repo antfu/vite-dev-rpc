f217a96f9759fda9d150e75967b0ca193fd531a7
Anthony Fu <anthonyfu117@hotmail.com>
2023-04-06 09:24:32 +0200

    chore: update deps

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,4 +1,4 @@
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 import type { Plugin } from 'vite'
 import { defineConfig } from 'vite'
 import { createRPCServer } from 'vite-dev-rpc'
